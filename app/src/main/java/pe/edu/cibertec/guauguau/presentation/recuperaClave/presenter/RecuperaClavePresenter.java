5c113d4d7957b8e4fa6dc4728f7486e267d69d8a
christian escobar <christian.mahlj@gmail.com>
2019-08-10 20:18:33 -0500

    Pantalla de Menu

---
+++ app/src/main/java/pe/edu/cibertec/guauguau/presentation/recuperaClave/presenter/RecuperaClavePresenter.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.guauguau.presentation.recuperaClave.presenter;
+
+public class RecuperaClavePresenter {
+}
---
+++ app/src/main/java/pe/edu/cibertec/guauguau/presentation/registrarce/presenter/RegistrarcePresenter.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.guauguau.presentation.registrarce.presenter;
+
+public class RegistrarcePresenter {
+}
