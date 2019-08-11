7711fa41a10842ed3fbd2609ebaaf53b6c630b5a
christian escobar <christian.mahlj@gmail.com>
2019-08-11 00:26:10 -0500

    Fondo Menu

---
+++ app/src/main/java/pe/edu/cibertec/guauguau/presentation/menu/IMenuContract.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.guauguau.presentation.menu;
+
+public interface IMenuContract {
+}
--- app/src/main/java/pe/edu/cibertec/guauguau/presentation/registrarce/IRegistrarceContrac.java
+++
@@ -1,4 +0,0 @@
-package pe.edu.cibertec.guauguau.presentation.registrarce;
-
-public interface IRegistrarceContrac {
-}
